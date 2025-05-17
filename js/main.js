266f3a8b3e4bfd772d21d9fce5ce0cf9857e6ef1
sharopov2014 <sharopovalizod@gmail.com>
2025-05-17 16:03:12 +0500

    first commit

--- js/main.js
+++ js/main.js
@@ -14,3 +14,11 @@ $(".owl-carousel").owlCarousel({
     },
   },
 });
+document.addEventListener("DOMContentLoaded", () => {
+  const menuToggle = document.getElementById("hamburger");
+  const navMenu = document.getElementById("nav-menu");
+
+  menuToggle.addEventListener("click", () => {
+    navMenu.classList.toggle("active");
+  });
+});
